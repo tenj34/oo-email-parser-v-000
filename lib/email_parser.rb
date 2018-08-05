58bb21934e3edc5c9e1d247561a516bb53379158
Juan Tenezaca <tenj34@gmail.com>
2018-08-05 15:24:18 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailParser
 
   def parse
     email_arrays = [] # here we will hold our parsed emails
-    email_arrays = @emails.split[/[,\s]/) # splitting the email when we see a comma or a space
+    email_arrays = @emails.split()/[,\s]/) # splitting the email when we see a comma or a space
     email_array.uniq #removing duplicates from an array
 
   end
